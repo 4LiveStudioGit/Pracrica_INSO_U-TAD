9498b2249ab3566d0ed835ea511ecbb8c38d5f77
4LiveStudioGit <105469548+4LiveStudioGit@users.noreply.github.com>
2024-01-19 14:34:29 +0100

    Creacion doc
    Hemos creado un nuevo documento de word

--- ProgramacionEstructurada/Practica_GIT.cpp
+++ ProgramacionEstructurada/Practica_GIT.cpp
@@ -1,2 +1,4 @@
 /*Esto es una prueba para empezar a modificar la rama "Prueba" del repositorio en Github. 
-Este es el cambio que vamos a realizar dentro de la rama "MAIN".*/
+Este es el cambio que vamos a realizar dentro de la rama "MAIN".
+Este es el cambio que estamos realizando
+*/
