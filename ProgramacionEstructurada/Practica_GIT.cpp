b3841ad4df7fc649def61e66adb90b31451282b7
4LiveStudioGit <david.alamo@gmail.com>
2024-01-19 13:25:33 +0100

    Cambio en el fichero de pruebas
    Este es el cambio que realizamos sobre la rama MAIN

--- ProgramacionEstructurada/Practica_GIT.cpp
+++ ProgramacionEstructurada/Practica_GIT.cpp
@@ -1,1 +1,2 @@
-/*Esto es una prueba para empezar a modificar la rama "Prueba" del repositorio en Github*/
+/*Esto es una prueba para empezar a modificar la rama "Prueba" del repositorio en Github. 
+Este es el cambio que vamos a realizar dentro de la rama "MAIN".*/
